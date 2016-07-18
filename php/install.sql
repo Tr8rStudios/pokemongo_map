c2cd4354cf81334e47e1ba0833a978a994b5e635
Eric Jiang <ericjiang97@gmail.com>
2016-07-18 15:06:39 +1000

    sqltable population commands

--- php/install.sql
+++ php/install.sql
@@ -1,7 +1,67 @@
-CREATE TABLE `mappoints` (
-  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-  `name` VARCHAR( 60 ) NOT NULL ,
-  `address` VARCHAR( 80 ) NOT NULL ,
+CREATE TABLE `gyms` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,    # Primary Key
+  `name` VARCHAR( 60 ) NOT NULL ,                   # Name of Point
+  `description` VARCHAR( 80 ) NOT NULL ,
+  `lat` FLOAT( 10, 6 ) NOT NULL ,
+  `lng` FLOAT( 10, 6 ) NOT NULL
+) ENGINE = MYISAM ;
+
+CREATE TABLE `pokestops` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,    # Primary Key
+  `name` VARCHAR( 60 ) NOT NULL ,                   # Name of Point
+  `description` VARCHAR( 80 ) NOT NULL ,
+  `lat` FLOAT( 10, 6 ) NOT NULL ,
+  `lng` FLOAT( 10, 6 ) NOT NULL
+) ENGINE = MYISAM ;
+
+
+CREATE TABLE `pokemon25` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,    # Primary Key
+  `name` VARCHAR( 60 ) NOT NULL ,                   # Name of Point
+  `description` VARCHAR( 80 ) NOT NULL ,
+  `lat` FLOAT( 10, 6 ) NOT NULL ,
+  `lng` FLOAT( 10, 6 ) NOT NULL
+) ENGINE = MYISAM ;
+
+
+CREATE TABLE `pokemon50` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,    # Primary Key
+  `name` VARCHAR( 60 ) NOT NULL ,                   # Name of Point
+  `description` VARCHAR( 80 ) NOT NULL ,
+  `lat` FLOAT( 10, 6 ) NOT NULL ,
+  `lng` FLOAT( 10, 6 ) NOT NULL
+) ENGINE = MYISAM ;
+
+
+CREATE TABLE `pokemon75` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,    # Primary Key
+  `name` VARCHAR( 60 ) NOT NULL ,                   # Name of Point
+  `description` VARCHAR( 80 ) NOT NULL ,
+  `lat` FLOAT( 10, 6 ) NOT NULL ,
+  `lng` FLOAT( 10, 6 ) NOT NULL
+) ENGINE = MYISAM ;
+
+
+CREATE TABLE `pokemon100` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,    # Primary Key
+  `name` VARCHAR( 60 ) NOT NULL ,                   # Name of Point
+  `description` VARCHAR( 80 ) NOT NULL ,
+  `lat` FLOAT( 10, 6 ) NOT NULL ,
+  `lng` FLOAT( 10, 6 ) NOT NULL
+) ENGINE = MYISAM ;
+
+CREATE TABLE `pokemon125` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,    # Primary Key
+  `name` VARCHAR( 60 ) NOT NULL ,                   # Name of Point
+  `description` VARCHAR( 80 ) NOT NULL ,
+  `lat` FLOAT( 10, 6 ) NOT NULL ,
+  `lng` FLOAT( 10, 6 ) NOT NULL
+) ENGINE = MYISAM ;
+
+CREATE TABLE `pokemon150` (
+  `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,    # Primary Key
+  `name` VARCHAR( 60 ) NOT NULL ,                   # Name of Point
+  `description` VARCHAR( 80 ) NOT NULL ,
   `lat` FLOAT( 10, 6 ) NOT NULL ,
   `lng` FLOAT( 10, 6 ) NOT NULL
 ) ENGINE = MYISAM ;
