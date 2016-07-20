a511ec914abc13ffdfcb2f9afac5fdd174ec8009
Eric Jiang <ericjiang97@gmail.com>
2016-07-20 13:48:17 +1000

    fixing some other issues, where document.onload() is notfiring properly
    moved to jquery instead

--- beta/js/map.js
+++ beta/js/map.js
@@ -1,68 +1,53 @@
-window.onload = function()
-{
+$( document ).ready(function() {
     var plain = new ol.layer.Tile({
         source: new ol.source.OSM()
-});
-/*
-var layer1=new ol.layer.Vector({
-          source: new ol.source.Vector({
-          url: 'data/test.json',
-          format: new ol.format.GeoJSON({
-          defaultDataProjection :'EPSG:4326', 
-          projection: 'EPSG:3857'
-            })
-        }),
-        name: 'NAME 1',
-        style: style_1()
-});*/
+    });
 
-var Test = new ol.layer.Vector({
-      title: 'added Layer',
-      source: new ol.source.Vector({
-         format: new ol.format.GeoJSON(),
-         url: 'data/test.json'
-      })
-})
-
-var map = new ol.Map({
-    target: 'map',
-    layers: [plain,Test],
-    view: new ol.View({
-    center: ol.proj.fromLonLat([-164,0]),
-    zoom: 2.1
+    var Test = new ol.layer.Vector({
+        title: 'added Layer',
+        source: new ol.source.Vector({
+            format: new ol.format.GeoJSON(),
+            url: 'data/test.json'
+        })
     })
-});
 
+    var map = new ol.Map({
+        target: 'map',
+        layers: [plain,Test],
+        view: new ol.View({
+        center: ol.proj.fromLonLat([-164,0]),
+        zoom: 2.1
+        })
+    });
 
-
-var displayFeatureInfo = function(pixel) {
-var features = [];
-map.forEachFeatureAtPixel(pixel, function(feature, layer) {
-    features.push(feature);
-});
-if (features.length > 0) {
-    var info = [];
-    var i, ii;
-    for (i = 0, ii = features.length; i < ii; ++i) {
-    info.push(features[i].get('name'));
+    var displayFeatureInfo = function(pixel) {
+    var features = [];
+    map.forEachFeatureAtPixel(pixel, function(feature, layer) {
+        features.push(feature);
+    });
+    if (features.length > 0) {
+        var info = [];
+        var i, ii;
+        for (i = 0, ii = features.length; i < ii; ++i) {
+        info.push(features[i].get('name'));
+        }
+        document.getElementById('info').innerHTML = info.join(', ') || '(unknown)';
+        map.getTarget().style.cursor = 'pointer';
+    } else {
+        document.getElementById('info').innerHTML = '&nbsp;';
+        map.getTarget().style.cursor = '';
     }
-    document.getElementById('info').innerHTML = info.join(', ') || '(unknown)';
-    map.getTarget().style.cursor = 'pointer';
-} else {
-    document.getElementById('info').innerHTML = '&nbsp;';
-    map.getTarget().style.cursor = '';
-}
-};
+    };
 
-map.on('pointermove', function(evt) {
-if (evt.dragging) {
-    return;
-}
-var pixel = map.getEventPixel(evt.originalEvent);
-displayFeatureInfo(pixel);
-});
+    map.on('pointermove', function(evt) {
+    if (evt.dragging) {
+        return;
+    }
+    var pixel = map.getEventPixel(evt.originalEvent);
+    displayFeatureInfo(pixel);
+    });
 
-map.on('click', function(evt) {
-    displayFeatureInfo(evt.pixel);
+    map.on('click', function(evt) {
+        displayFeatureInfo(evt.pixel);
+    });
 });
-}
